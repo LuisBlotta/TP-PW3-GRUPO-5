1df44c4cc0bdc666ee7a2aba87ae5d431a061030
Ian Tries <ian_tries@hotmail.com>
2021-05-06 21:33:34 -0300

    Se implementa primera prueba de controlador de Articulo/Mostrar.cshtml

---
+++ TP-PW3-GRUPO-5/TP-PW3-GRUPO-5/Controllers/ArticuloController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace TP_PW3_GRUPO_5.Controllers
+{
+    public class ArticuloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Mostrar()
+        {
+            Articulo miArticulo = new Articulo();
+            miArticulo.IdArticulo = 1;
+            miArticulo.Codigo = 123;
+            return View(miArticulo);
+        }
+    }
+}
