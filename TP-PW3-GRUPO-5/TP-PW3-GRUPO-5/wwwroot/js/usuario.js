420710bbf23e5ee88a8e60a0773d49e5bd33e4ea
LuisBlotta <luis.blotta@mdevz.com>
2021-05-13 20:03:59 -0300

    Fix paginado

--- TP-PW3-GRUPO-5/TP-PW3-GRUPO-5/wwwroot/js/usuario.js
+++ TP-PW3-GRUPO-5/TP-PW3-GRUPO-5/wwwroot/js/usuario.js
@@ -3,52 +3,63 @@ const btnEliminar = document.getElementById('eliminado');
 const inpNombre = document.getElementById('nombre');
 const inpEmail = document.getElementById('email');
 
-    function generarTabla(resultados){
+function generarTabla(resultados) {
 
-        listado = '';
+    var table = $('#example').DataTable();
+    table.destroy();
 
-        resultados.forEach(resultado => {
-            listado += `<tr><td>
+    listado = '';
+
+    resultados.forEach(resultado => {
+        listado += `<tr><td>
                 ${resultado.Nombre}
+                </td>
+                <td>
+                ${resultado.Apellido}
                 </td>
                 <td>
                 ${resultado.Email}
                 </td>
                 <td>
-                ${resultado.Apellido}
-                </td>
-                <td class="btn-reserva">
-                <a class='' href=#>Ver</a>
-                </td></tr>`
-        })
-        $('#tbody').html(listado);
+                <a class="me-3" href="/usuario/detalleUsuario?accion=ver" data-toggle="tooltip"
+                    title="Ver"><i class="fas fa-eye"></i></a>
+                <a class="me-3" href="/usuario/detalleUsuario?accion=editar" data-toggle="tooltip"
+                    title="Editar"><i class="fas fa-edit"></i></a>
+                <a href="#" data-toggle="tooltip" title="Borrar">
+                    <i class="fas fa-trash-alt"></i>
+                </a>
+                </td>        
+        </tr>`
+    })
+    $('#tbody').html(listado);
 
-    };
+};
 
 //Obtener resultados filtrados
 
-    function obtenerResultados() {
+function obtenerResultados() {
 
-        let data = {
-            Nombre: document.getElementById('nombre').value,
-            Email: document.getElementById('email').value,
-            Eliminado: true
-        }
+    let data = {
+        Nombre: document.getElementById('nombre').value,
+        Email: document.getElementById('email').value,
+        Eliminado: true
+    }
 
-        fetch('https://localhost:44344/usuario/ObtenerFiltros', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(data)
+    fetch('https://localhost:44344/usuario/ObtenerFiltros', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response =>
+            response.json()
+
+        )
+        .then(data => {
+            generarTabla(data)
+            paginacion();
         })
-            .then(response =>
-                response.json()
-
-            )
-            .then(data => {
-                generarTabla(data)
-            })
 }
 
 
@@ -58,29 +69,31 @@ $(document).ready(function () {
 });
 
 ////Paginacion
-//$(document).ready(function () {
-//    $('#example').DataTable();
-    
-//});
 
-$(document).ready(function () {
+
+function paginacion() {
+
     $('#example').DataTable({
+        retrieve: true,
+        destroy: true,
         "language": {
             "paginate": {
-            "first": "Primera",
-            "last": "Ultima",
-            "next": "Siguiente",
-            "previous": "Anterior"
-        },
+                "first": "Primera",
+                "last": "Ultima",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            },
             "lengthMenu": "Mostrar _MENU_ resultados por página",
             "zeroRecords": "No se encontraron resultados",
             "info": "Mostrando página  _PAGE_ de _PAGES_",
             "infoEmpty": "Sin resultados",
-            "infoFiltered": "(filtered from _MAX_ total records)",
-
+            "infoFiltered": "(filtered from _MAX_ total records)"
         }
-        
     });
     $('#example_filter').hide();
+}
+
+$(document).ready(function () {
+    paginacion();
 });
 
